e3ec361f12d11e5b92734eb02c22a72b71ef34e4
Muhammad Idrees <47222718+muhammadidreesshaikh@users.noreply.github.com>
2022-07-09 14:24:52 +0500

    added keys

---
+++ keys.ts
@@ -0,0 +1,9 @@
+export const firebaseKeys = {
+    apiKey: "AIzaSyBfC40nPaypWP_1qV1ZgKi0eT7LhHSukLA",
+    authDomain: "prime-plastics-56207.firebaseapp.com",
+    projectId: "prime-plastics-56207",
+    storageBucket: "prime-plastics-56207.appspot.com",
+    messagingSenderId: "426673181163",
+    appId: "1:426673181163:web:38689383bbf5953f4589ae",
+    measurementId: "G-XMPS0Z6SN4"
+}
