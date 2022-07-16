f85b11199f48250c03b33f4d7897a5ba136cd81e
Muhammad Idrees <47222718+muhammadidreesshaikh@users.noreply.github.com>
2022-07-16 20:07:44 +0500

    year in footer fix

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  currentDate = new Date();
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
